dd878fb68d846e3c511a6180ad866432a2d004ce
Oliver Knox <oliver@knox.gb.net>
2024-08-27 17:58:23 +0100

    Keep tenants in home page
    - Remove fragment
    - Remove domain models

--- src/main/java/net/gb/knox/portfolio/domain/Image.java
+++
@@ -1,4 +0,0 @@
-package net.gb.knox.portfolio.domain;
-
-public record Image(String src, String alt) {
-}
--- src/main/java/net/gb/knox/portfolio/domain/Tenant.java
+++
@@ -1,4 +0,0 @@
-package net.gb.knox.portfolio.domain;
-
-public record Tenant(Image img, String title, String description) {
-}
